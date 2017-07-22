68cc929e038bfd48bbac9c0172e64e983d859909
Paul Zaremba <zaremba@tonekids.com>
2017-07-22 14:25:06 -0500

    Changed to pillow import for PIL and got radar canvas up and running.

--- tonekids/nwsapi/ui_canvas_radar.py
+++ tonekids/nwsapi/ui_canvas_radar.py
@@ -5,6 +5,7 @@ Created on Jul 22, 2017
 '''
 
 import Tkinter as tk
+from PIL import ImageTk, Image
 
 class UiCanvasRadar(tk.Canvas, object):
     '''
@@ -17,5 +18,7 @@ class UiCanvasRadar(tk.Canvas, object):
         '''
         self.station_id = station_id
         tk.Canvas.__init__(self, master=master, *args, **kwargs)
-        self.config(width=600, height=550, bg="#333333")
-    
+        self.config(width=600, height=550, bg="#333333", cursor='crosshair')
+        #test_topo_img = pil.Image('/Users/zaremba/tmp/nwsapi/Overlays/LOT_Topo_Short.jpg')
+        self.test_topo_pi = ImageTk.PhotoImage(Image.open('/Users/zaremba/tmp/nwsapi/Overlays/LOT_Topo_Short.jpg'))
+        self.create_image((0,0), anchor=tk.NW, image=self.test_topo_pi)
